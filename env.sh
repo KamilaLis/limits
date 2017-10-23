83cc41b4e333403db37dd15f50c5cf9265af1e47
KamilaLis <kamila.lis@outlook.com>
2017-10-23 21:49:25 +0200

    add

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@
 
 source /home/stero/ws_ids/devel/setup.bash
 export ROS_IP=192.168.1.122
-export ROS_MASTER_URI=http://192.168.1.122:11311/
+export ROS_MASTER_URI=http://192.168.1.241:11311/
 exec "$@"
